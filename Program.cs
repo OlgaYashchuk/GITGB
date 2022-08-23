b2f4f83237de191088d1e1866a1437983b3586c2
OlgaYashchuk <87386462+OlgaYashchuk@users.noreply.github.com>
2022-08-23 21:00:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число A ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите целое число B ");
+        int B = Convert.ToInt32(Console.ReadLine());
+        if(A-B==0)
+        {
+             Console.WriteLine("равны");
+        }
+        if(A-B>0)
+        {
+            Console.WriteLine (A);
+        }
+        if(A-B<0)
+        {
+            Console.WriteLine (B);
+        }    
+
+    }
+}
